08d08c0d8d0c9921a15caa26cbd37eec3b167030
Ecem <48857703+ecemyaman@users.noreply.github.com>
2020-05-28 15:00:25 +0300

    Add files via upload

--- Tekno Eğitim/tekno_egitim_web.coreproject/Controllers/HomeController.cs
+++ Tekno Eğitim/tekno_egitim_web.coreproject/Controllers/HomeController.cs
@@ -3,22 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using tekno_egitim_web.coreproject.Data_Transfer_Objects;
 
-namespace tekno_egitim_web.coreproject.Controllers
+namespace tekno_egitim.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
+            ViewData["Title"] = "Anasayfa";
+            return View();
+        }
+        public IActionResult Hakkimizda()
+        {
+            ViewData["Title"] = "Hakkimizda";
+            return View();
+        }
+        public IActionResult Kurs()
+        {
+            ViewData["Title"] = "Enler";
+            return View();
+        }
+        public IActionResult Etkinlik()
+        {
+            ViewData["Title"] = "Etkinlikler";
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            ViewData["Title"] = "Üniversiteler";
+            return View();
+        }
+        public IActionResult Iletisim()
+        {
+            ViewData["Title"] = "İletisim";
             return View();
         }
 
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error(ErrorDto errordto)
-        {
-            return View(errordto);
-        }
     }
 }
---
+++ Tekno Eğitim/tekno_egitim_web.coreproject/KategoriAPI/KategoriAPIServices.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using tekno_egitim_web.coreproject.Data_Transfer_Objects;
+
+namespace tekno_egitim_web.coreproject.KategoriAPI
+{
+    public class KategoriAPIServices
+    {
+        private readonly HttpClient _httpclient;
+        public KategoriAPIServices(HttpClient httpclient)
+        {
+            _httpclient = httpclient;
+        }
+        public async Task<IEnumerable<KategoriDto>> GetAllAsync()
+        {
+            IEnumerable<KategoriDto> kategoridto;
+            var response = await _httpclient.GetAsync("kategoriler");
+            if (response.IsSuccessStatusCode)
+            {
+                kategoridto = JsonConvert.DeserializeObject<IEnumerable<KategoriDto>>
+                    (await response.Content.ReadAsStringAsync());
+            }
+
+            else
+            {
+                kategoridto = null;
+            }
+
+            return kategoridto;
+        }
+        public async Task<KategoriDto> AddAsync(KategoriDto KategoriDto)
+        {
+            var stringContent = new StringContent(JsonConvert.SerializeObject(KategoriDto), Encoding.UTF8, "application/json");
+
+            var response = await _httpclient.PostAsync("kategoriler", stringContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                KategoriDto = JsonConvert.DeserializeObject<KategoriDto>(await response.Content.ReadAsStringAsync());
+
+                return KategoriDto;
+            }
+            else
+            {
+                //loglama yap
+                return null;
+            }
+        }
+
+        public async Task<KategoriDto> GetByIdAsync(int id)
+        {
+            var response = await _httpclient.GetAsync($"kategoriler/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return JsonConvert.DeserializeObject<KategoriDto>(await response.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<bool> Update(KategoriDto KategoriDto)
+        {
+            var stringContent = new StringContent(JsonConvert.SerializeObject(KategoriDto), Encoding.UTF8, "application/json");
+
+            var response = await _httpclient.PutAsync("kategoriler", stringContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> Remove(int id)
+        {
+            var response = await _httpclient.DeleteAsync($"kategoriler/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
