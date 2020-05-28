245c65101b5b0e30b334a25c6af72e905b9c6df5
rrumeysagedik <48915929+rrumeysagedik@users.noreply.github.com>
2020-05-28 18:33:52 +0300

    Add files via upload

---
+++ Üye-Admin Panel/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email Adresi")]
+        [Required(ErrorMessage = "Email girilmesi zorunludur.")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Şifre girilmesi zorunludur.")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Şifreniz en az 4 karakterli olmalıdır.")]
+        public string Password { get; set; }
+
+    }
+}
---
+++ Üye-Admin Panel/ViewModels/UserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required(ErrorMessage = "Kullanıcı İsmi Gereklidir.")]
+        [Display(Name = "Kullanıcı Adı")]
+        public string UserName { get; set; }
+        [Display(Name = "Tel No:")]
+        public string PhoneNumber { get; set; }
+        
+        [Required(ErrorMessage = "Email Gereklidir.")]
+        [Display(Name = "Email Adresi")]
+        [EmailAddress(ErrorMessage = "Email Adresiniz Doğru Formatta Değil.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Şifre Gereklidir.")]
+        [Display(Name = "Şifre")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
