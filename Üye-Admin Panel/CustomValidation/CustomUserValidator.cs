c114b4c1857efb27983c88195145c8f30c1689b0
rrumeysagedik <48915929+rrumeysagedik@users.noreply.github.com>
2020-05-28 18:34:32 +0300

    Add files via upload

---
+++ Üye-Admin Panel/CustomValidation/CustomUserValidator.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.CustomValidation
+{
+    public class CustomUserValidator : IUserValidator<AppUser>
+    {
+        public Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user)
+        {
+
+            List<IdentityError> errors = new List<IdentityError>();
+              if (user.UserName.ToString() != user.Email.Split("@")[0])
+            {
+                errors.Add(new IdentityError()
+                {
+                    Code = "UserNameMustContainTheBegginingOfTheMail",
+                    Description = "Kullanıcı adınız, Emailinizde ki @ işaretinden önceki kısım olmalıdır."
+                });
+            }
+            if (errors.Count == 0)
+            {
+                return Task.FromResult(IdentityResult.Success);
+            }
+
+            else
+            {
+                return Task.FromResult(IdentityResult.Failed(errors.ToArray()));
+            }
+
+        }
+    }
+}
