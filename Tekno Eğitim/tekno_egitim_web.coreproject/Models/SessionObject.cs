f4da57419253438ac9750ed45b963ea4e54c3d98
beyza6127 <58363329+beyza6127@users.noreply.github.com>
2020-06-21 12:34:49 +0300

    Add files via upload

---
+++ Tekno Eğitim/tekno_egitim_web.coreproject/Models/SessionObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tekno_egitim_web.core.Models;
+
+namespace tekno_egitim_web.coreproject.Models
+{
+    public static class SessionObject
+    {
+        public static List<Universite> _universiteler { get; set; }
+    }
+}
